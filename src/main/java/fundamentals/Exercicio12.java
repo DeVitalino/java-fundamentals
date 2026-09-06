0235fba56624ab51cd0608cf99bc97ba2e451210
Mateus Vitalino <Vitalinojsus@hotmail.com>
2026-09-05 22:36:18 -0300

    More tasks

--- src/main/java/fundamentals/Exercicio12.java
+++ src/main/java/fundamentals/Exercicio12.java
@@ -1,4 +1,26 @@
 package fundamentals;
 
-public class Exercicio12 {
-}
+import java.util.Scanner;
+
+    public class Exercicio12 {
+
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.print("Digite um número: ");
+            int numero = scanner.nextInt();
+
+            int invertido = 0;
+
+            while (numero != 0) {
+                int ultimoDigito = numero % 10;
+                invertido = invertido * 10 + ultimoDigito;
+                numero = numero / 10;
+            }
+
+            System.out.println("Número invertido: " + invertido);
+
+            scanner.close();
+        }
+    }
+
